f768655f3401eb994d5f4e81b7cfcf17ffe203e1
Dcrown <aisimiyuoluwadara@gmail.com>
2022-05-20 12:00:26 +0100

    Update aux_stdlib.c

--- aux_stdlib.c
+++ aux_stdlib.c
@@ -1,199 +1,100 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * get_len - Get the lenght of a number.
- *
- *   * @n: type int number.
- *
- *    * Return: Lenght of a number.
- *
- *     */
-
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ * Return: Lenght of a number.
+ */
 int get_len(int n)
-
 {
+	unsigned int n1;
+	int lenght = 1;
 
-		unsigned int n1;
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
 
-			int lenght = 1;
-
-
-
-				if (n < 0)
-
-						{
-
-									lenght++;
-
-											n1 = n * -1;
-
-												}
-
-					else
-
-							{
-
-										n1 = n;
-
-											}
-
-						while (n1 > 9)
-
-								{
-
-											lenght++;
-
-													n1 = n1 / 10;
-
-														}
-
-
-
-							return (lenght);
-
+	return (lenght);
 }
-
 /**
- *
- *  * aux_itoa - function converts int to string.
- *
- *   * @n: type int number
- *
- *    * Return: String.
- *
- *     */
-
+ * aux_itoa - function converts int to string.
+ * @n: type int number
+ * Return: String.
+ */
 char *aux_itoa(int n)
-
 {
+	unsigned int n1;
+	int lenght = get_len(n);
+	char *buffer;
 
-		unsigned int n1;
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
 
-			int lenght = get_len(n);
+	*(buffer + lenght) = '\0';
 
-				char *buffer;
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
 
-
-
-					buffer = malloc(sizeof(char) * (lenght + 1));
-
-						if (buffer == 0)
-
-									return (NULL);
-
-
-
-							*(buffer + lenght) = '\0';
-
-
-
-								if (n < 0)
-
-										{
-
-													n1 = n * -1;
-
-															buffer[0] = '-';
-
-																}
-
-									else
-
-											{
-
-														n1 = n;
-
-															}
-
-
-
-										lenght--;
-
-											do {
-
-														*(buffer + lenght) = (n1 % 10) + '0';
-
-																n1 = n1 / 10;
-
-																		lenght--;
-
-																			}
-
-												while (n1 > 0)
-
-															;
-
-													return (buffer);
-
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	}
+	while (n1 > 0)
+		;
+	return (buffer);
 }
 
-
-
 /**
- *
- *  * _atoi - converts a string to an integer.
- *
- *   * @s: input string.
- *
- *    * Return: integer.
- *
- *     */
-
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
 int _atoi(char *s)
-
 {
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-		unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
+		if (*(s + count) == '-')
+			pn *= -1;
 
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
 
-			while (*(s + count) != '\0')
-
-					{
-
-								if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-
-												break;
-
-
-
-										if (*(s + count) == '-')
-
-														pn *= -1;
-
-
-
-												if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-
-															{
-
-																			if (size > 0)
-
-																								m *= 10;
-
-																						size++;
-
-																								}
-
-														count++;
-
-															}
-
-
-
-				for (i = count - size; i < count; i++)
-
-						{
-
-									oi = oi + ((*(s + i) - 48) * m);
-
-											m /= 10;
-
-												}
-
-					return (oi * pn);
-
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
