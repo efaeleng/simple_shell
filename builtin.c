c9454bb0547f92333eeddfc06f19a99d75a3d405
Efa-Iwa <gistend@gmail.com>
2022-05-19 17:55:51 -0700

    May change the current working directory

--- builtin.c
+++ builtin.c
@@ -5,194 +5,64 @@
  */
 
 #include "shell.h"
-int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
- * get_builtin - Matches a command with a corresponding
- *                shellby builtin function.
- *                @command: The command to match.
+ * env - prints the current_environnement
+ * @tokenized_command: command entered
  *
- *                Return: A function pointer to the corresponding builtin.
+ * Return: void
  */
-int (*get_builtin(char *command))(char **args, char **front)
+
+void env(char **tokenized_command __attribute__((unused)))
 {
-	builtin_t funcs[] = {
-		{ "exit", shellby_exit },
-		{ "env", shellby_env },
-		{ "setenv", shellby_setenv },
-		{ "unsetenv", shellby_unsetenv },
-		{ "cd", shellby_cd },
-		{ "alias", shellby_alias },
-		{ "help", shellby_help },
-		{ NULL, NULL }
-	};
 	int i;
 
-	for (i = 0; funcs[i].name; i++)
+	for (i = 0; environ[i] != NULL; i++)
 	{
-		if (_strcmp(funcs[i].name, command) == 0)
-			break;
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
 	}
-	return (funcs[i].f);
 }
 
 /**
- * shellby_exit - Causes normal process termination
- *                 for the shellby shell.
- *                 @args: An array of arguments containing the exit value.
- *                 @front: A double pointer to the beginning of args.
+ * quit - exits the shell
+ * @tokenized_command: command entered
  *
- *                 Return: If there are no arguments - -3.
- *                          If the given exit value is invalid - 2.
- *                                   O/w - exits with the given status value.
- *
- *                                   Description: Upon returning -3, the program exits back in the main function.
+ * Return: void
  */
-int shellby_exit(char **args, char **front)
+
+void quit(char **tokenized_command)
 {
-	int i, len_of_int = 10;
-	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
+	int num_token = 0, arg;
 
-	if (args[0])
+	for (; tokenized_command[num_token] != NULL; num_token++)
+				;
+	if (num_token == 1)
 	{
-		if (args[0][0] == '+')
-		{
-			i = 1;
-			len_of_int++;
-		}
-		for (; args[0][i]; i++)
-		{
-			if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
-				num = (num * 10) + (args[0][i] - '0');
-			else
-				return (create_error(--args, 2));
-		}
+		free(tokenized_command);
+		free(line);
+		free(commands);
+		exit(status);
 	}
-	else
+	else if (num_token == 2)
 	{
-		return (-3);
-	}
-	if (num > max - 1)
-		return (create_error(--args, 2));
-	args -= 1;
-	free_args(args, front);
-	free_env();
-	free_alias_list(aliases);
-	exit(num);
-}
-
-/**
- * shellby_cd - Changes the current directory of the shellby process.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
- *
- * Return: If the given string is not a directory - 2.
- *          If an error occurs - -1.
- *                   Otherwise - 0.
- */
-int shellby_cd(char **args, char __attribute__((__unused__)) **front)
-{
-	char **dir_info, *new_line = "\n";
-	char *oldpwd = NULL, *pwd = NULL;
-	struct stat dir;
-
-	oldpwd = getcwd(oldpwd, 0);
-	if (!oldpwd)
-		return (-1);
-
-	if (args[0])
-	{
-		if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
+		arg = _atoi(tokenized_command[1]);
+		if (arg == -1)
 		{
-			if ((args[0][1] == '-' && args[0][2] == '\0') ||
-					args[0][1] == '\0')
-			{
-				if (_getenv("OLDPWD") != NULL)
-					(chdir(*_getenv("OLDPWD") + 7));
-			}
-			else
-			{
-				free(oldpwd);
-				return (create_error(args, 2));
-			}
+			print(shell_name, STDERR_FILENO);
+			print(": 1: exit: Illegal number: ", STDERR_FILENO);
+			print(tokenized_command[1], STDERR_FILENO);
+			print("\n", STDERR_FILENO);
+			status = 2;
 		}
 		else
 		{
-			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
-					&& ((dir.st_mode & S_IXUSR) != 0))
-				chdir(args[0]);
-			else
-			{
-				free(oldpwd);
-				return (create_error(args, 2));
-			}
+			free(line);
+			free(tokenized_command);
+			free(commands);
+			exit(arg);
 		}
 	}
 	else
-	{
-		if (_getenv("HOME") != NULL)
-			chdir(*(_getenv("HOME")) + 5);
-	}
-
-	pwd = getcwd(pwd, 0);
-	if (!pwd)
-		return (-1);
-
-	dir_info = malloc(sizeof(char *) * 2);
-	if (!dir_info)
-		return (-1);
-
-	dir_info[0] = "OLDPWD";
-	dir_info[1] = oldpwd;
-	if (shellby_setenv(dir_info, dir_info) == -1)
-		return (-1);
-
-	dir_info[0] = "PWD";
-	dir_info[1] = pwd;
-	if (shellby_setenv(dir_info, dir_info) == -1)
-		return (-1);
-	if (args[0] && args[0][0] == '-' && args[0][1] != '-')
-	{
-		write(STDOUT_FILENO, pwd, _strlen(pwd));
-		write(STDOUT_FILENO, new_line, 1);
-	}
-	free(oldpwd);
-	free(pwd);
-	free(dir_info);
-	return (0);
-}
-
-/**
- * shellby_help - Displays information about shellby builtin commands.
- * @args: An array of arguments.
- * @front: A pointer to the beginning of args.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
- */
-int shellby_help(char **args, char __attribute__((__unused__)) **front)
-{
-	if (!args[0])
-		help_all();
-	else if (_strcmp(args[0], "alias") == 0)
-		help_alias();
-	else if (_strcmp(args[0], "cd") == 0)
-		help_cd();
-	else if (_strcmp(args[0], "exit") == 0)
-		help_exit();
-	else if (_strcmp(args[0], "env") == 0)
-		help_env();
-	else if (_strcmp(args[0], "setenv") == 0)
-		help_setenv();
-	else if (_strcmp(args[0], "unsetenv") == 0)
-		help_unsetenv();
-	else if (_strcmp(args[0], "help") == 0)
-		help_help();
-	else
-		write(STDERR_FILENO, name, _strlen(name));
-
-	return (0);
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
